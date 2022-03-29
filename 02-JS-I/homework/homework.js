cdead3e06262e9f84652cc338c85ac177d429c4f
MireyaLu <MireyaFreteslucero@hotmail.com>
2022-03-29 19:29:44 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = null;
 const nuevoNum = null;
 
 // Crea una variable booleana:
-const nuevoBool = "LOVE";
+const nuevoBool = "STRING";
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
